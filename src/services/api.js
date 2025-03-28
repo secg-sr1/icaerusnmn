7344113562cf32271af88e1626256daede16464c
secg-sr1 <scalgua89@gmail.com>
2025-03-27 22:08:31 -0600

    api services modified

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    timeout: 10000, // optional, for request timeouts
+    timeout: 60000, // optional, for request timeouts
 });
 
 // Optional: add interceptors for auth if needed later
